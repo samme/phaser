745017cc2d45b7770b14e250bbe3fe1e24550e0b
samme <samme@users.noreply.github.com>
2026-04-06 19:25:00 -0700

    load svg text

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -36,6 +36,10 @@ module.exports = [
                 {
                   test: /\.html$/,
                   type: 'asset/source'
+                },
+                {
+                    test: /\.svg$/,
+                    type: 'asset/source'
                 }
             ]
         },
