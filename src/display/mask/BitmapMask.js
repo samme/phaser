4aa1aa021fffc931b85ba2b92c406883f88e360b
samme <samme@users.noreply.github.com>
2026-04-08 18:31:06 -0700

    fix BitmapMask size, add maskPosition

--- src/display/mask/BitmapMask.js
+++ src/display/mask/BitmapMask.js
@@ -10,24 +10,31 @@ const BitmapMask = new Class({
 
     initialize:
 
-    function BitmapMask (scene, x, y, texture, maskSize = null, repeat = 'no-repeat', mode = 'alpha')
+    function BitmapMask (scene, x, y, texture, maskPosition = null, maskSize = null, repeat = 'no-repeat', mode = 'alpha')
     {
         this.scene = scene;
         this.x = x;
         this.y = y;
 
+        this.scaleX = 1;
+        this.scaleY = 1;
+
         this.setTexture(texture);
 
+        this.maskPosition = maskPosition;
         this.maskSize = maskSize;
         this.repeat = repeat;
         this.mode = mode;
+
+        this.name = 'mask';
     },
 
-    getCSSMask: function ()
+    getCSSValue: function ()
     {
+        const maskPosition = this.maskPosition || `${this.x}px ${this.y}px`;
         const maskSize = this.maskSize || `${this.displayWidth}px ${this.displayHeight}px`;
 
-        return `${this.frame.source.cssImage} ${this.x}px ${this.y}px / ${maskSize} ${this.repeat} ${this.mode}`;
+        return `${this.frame.source.cssImage} ${maskPosition} / ${maskSize} ${this.repeat} ${this.mode}`;
     }
 
 });
