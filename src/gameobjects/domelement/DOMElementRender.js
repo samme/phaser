d0f50d5cfddebdebdc5562992c96c4b4a6151956
samme <samme@users.noreply.github.com>
2026-04-15 10:37:08 -0700

    add DOMElementCSSRenderer

--- src/gameobjects/domelement/DOMElementRender.js
+++ src/gameobjects/domelement/DOMElementRender.js
@@ -4,23 +4,8 @@
  * @license      {@link https://opensource.org/licenses/MIT|MIT License}
  */
 
-var NOOP = require('../../utils/NOOP');
-var renderWebGL = NOOP;
-var renderCanvas = NOOP;
-
-if (typeof WEBGL_RENDERER)
-{
-    renderWebGL = require('./DOMElementCSSRenderer');
-}
-
-if (typeof CANVAS_RENDERER)
-{
-    renderCanvas = require('./DOMElementCSSRenderer');
-}
-
 module.exports = {
 
-    renderWebGL: renderWebGL,
-    renderCanvas: renderCanvas
+    renderCSS: require('./DOMElementCSSRenderer')
 
 };
