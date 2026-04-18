d4b145d36310f19101163972d300ab53f984a062
samme <samme@users.noreply.github.com>
2026-04-18 08:52:29 -0700

    add getRenderBounds()

--- src/gameobjects/components/CSSRenderNode.js
+++ src/gameobjects/components/CSSRenderNode.js
@@ -1,3 +1,5 @@
+const Rectangle = require('../../geom/rectangle/Rectangle');
+
 const RENDER_MASK = 15;
 
 const CSSRenderNode = {
@@ -33,6 +35,34 @@ const CSSRenderNode = {
     willRenderCSS: function ()
     {
         return RENDER_MASK === this.renderFlags;
+    },
+
+    // Never use this.
+    getRenderBounds: function (output)
+    {
+        if (!output)
+        {
+            output = new Rectangle();
+        }
+
+        const { element } = this.renderNode;
+        const { cameras, renderer } = this.scene.sys;
+
+        if (!element.parentNode)
+        {
+            renderer.append(this.renderNode, cameras.main.worldViewRenderNode);
+        }
+
+        this.renderCSS(renderer, this, cameras.main);
+
+        const rect = element.getBoundingClientRect();
+
+        output.x = rect.left;
+        output.y = rect.top;
+        output.width = rect.width;
+        output.height = rect.height;
+
+        return output;
     }
 };
 
