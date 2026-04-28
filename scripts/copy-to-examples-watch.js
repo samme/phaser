9f8ad042079609ee7698d05b341f49650b8949ff
samme <samme@users.noreply.github.com>
2026-04-28 10:34:22 -0700

    copy to phaser3-examples again

--- scripts/copy-to-examples-watch.js
+++ scripts/copy-to-examples-watch.js
@@ -2,9 +2,9 @@ let fs = require('fs-extra');
 
 let source = './build/phaser.js';
 let sourceMap = './build/phaser.js.map';
-let dest = '../examples/public/build/dev.js';
-let destDir = '../examples/public/build/';
-let destMap = '../examples/public/build/phaser.js.map';
+let dest = '../phaser3-examples/public/build/dev.js';
+let destDir = '../phaser3-examples/public/build/';
+let destMap = '../phaser3-examples/public/build/phaser.js.map';
 
 if (fs.existsSync(destDir))
 {
@@ -30,5 +30,5 @@ if (fs.existsSync(destDir))
 }
 else
 {
-    console.log('Copy-to-Examples failed: Phaser Examples not present at ../examples');
+    console.log('Copy-to-Examples failed: Phaser Examples not present at', destDir);
 }
