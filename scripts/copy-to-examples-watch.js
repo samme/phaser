20d2e4b2b925f57beb06c73acc11bcc7f0cd6974
samme <samme@users.noreply.github.com>
2026-05-16 10:07:36 -0700

    rename examples folder in watch

--- scripts/copy-to-examples-watch.js
+++ scripts/copy-to-examples-watch.js
@@ -2,9 +2,9 @@ let fs = require('fs-extra');
 
 let source = './build/phaser.js';
 let sourceMap = './build/phaser.js.map';
-let dest = '../phaser3-examples/public/build/dev.js';
-let destDir = '../phaser3-examples/public/build/';
-let destMap = '../phaser3-examples/public/build/phaser.js.map';
+let dest = '../examples/public/build/dev.js';
+let destDir = '../examples/public/build/';
+let destMap = '../examples/public/build/phaser.js.map';
 
 if (fs.existsSync(destDir))
 {
